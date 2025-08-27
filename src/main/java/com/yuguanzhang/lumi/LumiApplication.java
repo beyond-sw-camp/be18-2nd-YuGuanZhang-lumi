0d8438c71bb831c092c82062689bac8a144841d8
ksong2006 <ksong2006@naver.com>
2025-08-27 19:53:57 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/yuguanzhang/lumi/LumiApplication.java
@@ -0,0 +1,13 @@
+package com.yuguanzhang.lumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumiApplication.class, args);
+	}
+
+}
