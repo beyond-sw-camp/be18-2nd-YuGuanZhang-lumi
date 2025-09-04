8b21ff051c0c2b5f7a4f72a9cd3671d7288ceed5
ydg010 <ydg010@skuniv.ac.kr>
2025-09-04 15:40:35 +0900

    어노테이션 필요한 파일 추가

--- src/main/java/com/yuguanzhang/lumi/LumiApplication.java
+++ src/main/java/com/yuguanzhang/lumi/LumiApplication.java
@@ -2,12 +2,14 @@ package com.yuguanzhang.lumi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing //자동 createAt 설정하려고 추가했음
 public class LumiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LumiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LumiApplication.class, args);
+    }
 
 }
