84aa0e59cab93b1e06cba91015350ae17b1b8b96
Jo YongJu <whwjyj00@naver.com>
2025-09-05 10:31:29 +0900

    feat: 시큐리티 수정

--- src/main/java/com/yuguanzhang/lumi/common/config/SecurityConfig.java
+++ src/main/java/com/yuguanzhang/lumi/common/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -41,7 +42,7 @@ public class SecurityConfig {
     // SecurityFilterChain Bean
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()) // CSRF 비활성화
+        http.csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
                 .cors(cors -> {
                 })             // CORS 허용
                 .sessionManagement(session -> session.sessionCreationPolicy(
