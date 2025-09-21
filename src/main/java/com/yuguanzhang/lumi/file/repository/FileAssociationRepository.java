a1a795583b0445496dcb719af88eb0367b5561f4
ydg010 <97106031+ydg010@users.noreply.github.com>
2025-09-21 22:37:20 +0900

    feat : 수업 자료 구현 (#35)
    충돌 해결

--- src/main/java/com/yuguanzhang/lumi/file/repository/FileAssociationRepository.java
+++ src/main/java/com/yuguanzhang/lumi/file/repository/FileAssociationRepository.java
@@ -1,7 +1,14 @@
 package com.yuguanzhang.lumi.file.repository;
 
 import com.yuguanzhang.lumi.file.entity.FileAssociation;
+import com.yuguanzhang.lumi.file.enums.EntityType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FileAssociationRepository extends JpaRepository<FileAssociation, Long> {
+    //id 와 type 으로 select
+    List<FileAssociation> findByEntityIdAndEntityType(Long entityId, EntityType entityType);
+
+    void deleteByEntityIdAndEntityType(Long materialId, EntityType entityType);
 }
