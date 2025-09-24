65ac8bc7c933e3a8c91bb65b6be60fb2637ecaac
ydg010 <ydg010@skuniv.ac.kr>
2025-09-24 16:17:32 +0900

    채널 dto에서 roleName포함하게 수정

--- src/main/java/com/yuguanzhang/lumi/channel/service/ChannelServiceImpl.java
+++ src/main/java/com/yuguanzhang/lumi/channel/service/ChannelServiceImpl.java
@@ -67,8 +67,7 @@ public class ChannelServiceImpl implements ChannelService {
     @Transactional(readOnly = true)
     public Page<ChannelsResponseDto> getChannels(User user, Pageable pageable) {
         //채널 가져오기
-        Page<Channel> channels = channelRepository.findByChannelUsers_User(user, pageable);
-
+        Page<ChannelUser> channels = channelUserRepository.findByUser(user, pageable);
 
         return channels.map(ChannelsResponseDto::fromEntity);
     }
